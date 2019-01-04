206193557b6b7d0576896e7071ef59f7566ac240
Glitch (brass-mask) <none>
2019-01-04 15:33:18 +0000

    👰👨‍❤️‍👨 Checkpoint ./routes/api.js:2505047/921

--- routes/api.js
+++ routes/api.js
@@ -18,19 +18,19 @@ const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRIN
 mongoose.connect(CONNECTION_STRING);
 const Schema = mongoose.Schema;
 
-  const issueSchema = new Schema({
-      project: {type:String,required:true},
-      issue_title: {type:String,required:true},
-      issue_text: {type:String,required:true},
-      created_by: {type:String,required:true},
-      assigned_to: {type:String},
-      status_text: {type:String},
-      created_on: {type:Date},
-      updated_on: {type:Date},
-      open: (boolean, true for open, false for closed)
-  }); 
+const issueSchema = new Schema({
+    project: {type:String,required:true},
+    issue_title: {type:String,required:true},
+    issue_text: {type:String,required:true},
+    created_by: {type:String,required:true},
+    assigned_to: {type:String},
+    status_text: {type:String},
+    created_on: {type:Date},
+    updated_on: {type:Date},
+    open: {type: Boolean}
+}); 
 
-const Person = mongoose.model("Person", personSchema);
+const Issue = mongoose.model("Issue", issueSchema);
 
 module.exports = function (app) {
 
@@ -42,14 +42,19 @@ module.exports = function (app) {
       //required issue_title, issue_text, created_by, and optional assigned_to and status_text.
       //console.log(req.body);
       const {issue_title, issue_text, created_by, assigned_to, status_text } = req.body
-      const newIssue = {
+      const newIssue = new Issue({
         project: project,
         issue_title: issue_title,
         issue_text: issue_text,
         created_by: created_by, 
         assigned_to: assigned_to || null,
         status_text: status_text || null
-      }
+      });
+      
+      newIssue.save(function(err, data) {
+          console.log(data);
+      });
+
     })
     
     .post(function (req, res){
